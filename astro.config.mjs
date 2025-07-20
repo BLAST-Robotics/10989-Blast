80365d0a282dcc5deb00652401b61790eb4de02b
virat2010 <59180983+vrobot-2@users.noreply.github.com>
2025-07-20 19:39:58 -0400

    Add Summer Camp popup and update Astro config
    Introduces a new SummerCampPopup component and image, updates the homepage and layout to include the popup, and enhances astro.config.mjs with sitemap, Partytown, and Vercel adapter integrations. Also removes pnpm-workspace.yaml and bun.lockb, and updates dependencies in package.json and bun.lock.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
+
+import partytown from '@astrojs/partytown';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://team10989.org',
+  prefetch: true,
+
+  vite: {
+      plugins: [tailwindcss(), sitemap()],
+    },
+
+  integrations: [partytown()],
+  adapter: vercel(),
 });
